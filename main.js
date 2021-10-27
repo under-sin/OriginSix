fca84773748cc86b6bab706920a0f1fe26f3752a
Anderson Vieira <andersonvieira818@yahoo.com.br>
2021-10-26 22:41:58 -0300

    finished project... or no

--- main.js
+++ main.js
@@ -82,3 +82,28 @@ window.addEventListener('scroll', function () {
     backToTopButton.classList.remove('show')
   }
 })
+
+// Menu ativo conforma a seção da página
+const sections = document.querySelectorAll('main section[id]')
+window.addEventListener('scroll', function () {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+})
